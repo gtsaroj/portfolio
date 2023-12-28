384be876a2b62c82e5d4d0fcb486797aab5bf251
Saroj <sarojgt326@gmail.com>
2023-12-28 20:50:33 +0545

    url changed

--- src/Works/Data.jsx
+++ src/Works/Data.jsx
@@ -3,58 +3,55 @@ import React from "react";
 export const ProjectData = [
   {
     id: 1,
-    image: require('./Projects/f1.png'),
+    image: require("./Projects/f1.png"),
     title: "Weather app ",
     category: "web development",
-    link: 'https://world-wide-weather-app1.netlify.app/',
-    github: 'https://github.com/gtsaroj/Weather-App',
+    link: "https://world-wide-weather-app1.netlify.app/",
+    github: "https://github.com/gtsaroj/Weather-App",
   },
   {
     id: 2,
-    image: require('./Projects/f2.png'),
+    image: require("./Projects/f2.png"),
     title: "x-mato",
     category: "Ecommerce",
-    link: 'https://x-mato.onrender.com',
-    github: 'https://github.com/gtsaroj/x-mato_frontend',
+    link: "https://x-mato.onrender.com",
+    github: "https://github.com/gtsaroj/x-mato_frontend",
   },
   {
     id: 3,
-    image: require('./Projects/f3.png'),
+    image: require("./Projects/f3.png"),
     title: "Tic Tac Toe",
     category: "Game development",
-    link: 'https://nepaligame.netlify.app/',
-    github: 'https://github.com/gtsaroj/Tic-Tac-Toe',
+    link: "https://nepaligame.netlify.app/",
+    github: "https://github.com/gtsaroj/Tic-Tac-Toe",
   },
   {
     id: 4,
-    image: require('./Projects/f4.png'),
+    image: require("./Projects/f4.png"),
     title: "graphstick nepal",
     category: "Ecommerce",
-    link: 'https://graphtick-online-shop.web.app/',
-    github: 'https://github.com/gtsaroj/graphstick-clientside',
-    
+    link: "https://graphtick-online-shop.web.app/",
+    github: "https://github.com/gtsaroj/graphstick-clientside",
   },
   {
     id: 5,
-    image: require('./Projects/f5.png'),
+    image: require("./Projects/f5.png"),
     title: "personal portfolio",
     category: "Portfolio",
-    link: 'sarojgt-coder-porfolio.onrender.com',
-    github: 'https://github.com/gtsaroj/portfolio',
+    link: "https://saroj-gt.web.app/",
+    github: "https://github.com/gtsaroj/portfolio",
   },
   {
     id: 6,
-    image: require('./Projects/f9.png'),
-    title: 'graphstick server',
-    category: 'Ecommerce',
-    link: 'https://graphstick-server-fk79.onrender.com/admin',
-    github: 'https://github.com/gtsaroj/graphstick-server',
-
-  }
- 
+    image: require("./Projects/f9.png"),
+    title: "graphstick server",
+    category: "Ecommerce",
+    link: "https://graphstick-server-fk79.onrender.com/admin",
+    github: "https://github.com/gtsaroj/graphstick-server",
+  },
 ];
 
-export const projectNav = [ 
+export const projectNav = [
   {
     name: "All",
   },
@@ -68,6 +65,6 @@ export const projectNav = [
     name: "Portfolio",
   },
   {
-    name: 'Game development'
-  }
+    name: "Game development",
+  },
 ];
